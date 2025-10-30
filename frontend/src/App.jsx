41040484e864177c1bfb6a9e705a3e0d0f5cdef5
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-30 09:30:19 -0300

    Adicionando fonte

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,9 @@
+import Home from "./components/Home"
+import Login from "./components/Login"
+import Sobre from "./components/Sobre"
+import Portifolio from "./components/Portifolio"
+import Contato from "./components/Contato"
+
 export default function App() {
   return (
     <>
--- frontend/src/components/Sobre.jsx
+++ frontend/src/components/Sobre.jsx
@@ -1,6 +1,8 @@
 const Sobre = () => {
   return (
-    <div>Sobre</div>
+    <section className="flex flex-col justify-center items-center bg-yellow-700">
+      <h1>Sobre Nós</h1>
+    </section>
   )
 }
 
